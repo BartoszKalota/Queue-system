608666466284e7e0fd97a75911e267bdc76c9386
Bartosz Kalota <bartek.kalota@wp.pl>
2020-11-19 16:18:57 +0100

    Removing extra exports and changing to arrow functions

--- src/constants/db.js
+++ src/constants/db.js
@@ -1,5 +1,1 @@
 export const PAGE_SIZE = 10;
-
-export default {
-  PAGE_SIZE,
-};
--- src/models/index.js
+++ src/models/index.js
@@ -2,14 +2,8 @@ import mongoose from 'mongoose';
 
 import { DB_ADDRESS, DB_NAME, DB_PORT } from '../config/db';
 
-const connectToMongoose = async () => {
+export const connectToMongoose = async () => {
   const url = `mongodb://${DB_ADDRESS}:${DB_PORT}/${DB_NAME}`;
 
   await mongoose.connect(url, { useNewUrlParser: true });
-};
-
-export { connectToMongoose };
-
-export default {
-  connectToMongoose,
 };
