58b3278412398abc5732650d2f88e92f4521837b
Bartosz Kalota <bartek.kalota@wp.pl>
2020-11-25 14:15:20 +0100

    mapQueueData as class method

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,17 +1,27 @@
 import {
   addToQueue as dbAddToQueue
 } from '../models/queue';
-import mapQueueData from '../utils/mapQueueData';
 
 import { VALIDATION_ERROR } from '../constants/error';
 import { errorIdValidation } from '../utils/errorValidationService';
 
+import { mapQueueDataInterface } from '../../types/interfaces';
+
 export default class Client {
+  // Internal filter
+  mapQueueData({ members, _id, name }: mapQueueDataInterface) {
+    return {
+      name,
+      id: _id,
+      length: members.length
+    };
+  };
+
   async addToQueue(queueId: string): Promise<any> {
     errorIdValidation(queueId, VALIDATION_ERROR);
 
     const queueData = await dbAddToQueue(queueId, '');
-    const queueDataForClient = mapQueueData(queueData);
+    const queueDataForClient = this.mapQueueData(queueData);
     return {
       userId: queueData.members[queueData.members.length - 1],
       ...queueDataForClient
--- src/utils/mapQueueData.ts
+++
@@ -1,7 +0,0 @@
-import { mapQueueDataInterface } from '../../types/interfaces';
-
-export default ({ members, _id, name }: mapQueueDataInterface) => ({
-  name,
-  id: _id,
-  length: members.length
-});
